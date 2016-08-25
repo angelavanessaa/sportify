7c9aa437ba6d91ebe9ac34455049b5cbe103c9a8
Angela <Vanessa>
2016-08-25 15:37:07 +0700

    can register, cannot login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'home#index'
 
   devise_for :users
+  # resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
