3c6456bc0440a138417305d599adbc482e88db41
ftnpdrs93 <fatinpidrus@yahoo.com>
2016-08-27 12:32:50 +0800

    index page and create wishlist page is partially done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :category
+  has_many :wishlists
+  has_many :users, :through => :wishlists 
 end
---
+++ db/migrate/20160826072710_add_column_to_votes.rb
@@ -0,0 +1,5 @@
+class AddColumnToVotes < ActiveRecord::Migration
+  def change
+    add_column :votes, :count, :integer, default: 0
+  end
+end
