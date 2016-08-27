02fee116f8c79e1b4ac85f599886a9ae44b906af
James Loh <jameslo_008@hotmail.com>
2016-08-27 14:55:56 +0800

    omni

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
+
 
 
 end
-=======
-end
->>>>>>> 09715cd7a14ea6abe05db6aa63bc0d660917c6b0
