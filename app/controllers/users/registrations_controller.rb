4b0f97bd1d1e3c117e85d99d545ac41c14a1e766
m3y2x1 <m3y2x1@gmail.com>
2016-08-28 08:37:26 +0800

    image uploader fixed for user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -58,6 +58,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
   def after_sign_in_path_for(resource)
+    
     stored_location_for(resource) || current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :lockable, :timeoutable, :omniauthable, :omniauth_providers => [:facebook]
          #:confirmable, 
          #:omniauthable
+   has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 	  def self.from_omniauth(auth)
 	    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
