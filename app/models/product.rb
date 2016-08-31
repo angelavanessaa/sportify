26f3e11d2671ccd703a0de47eafbcd411ee1c78d
Angela <Vanessa>
2016-09-01 05:36:34 +0700

    did the deals stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ActiveRecord::Base
   has_many :wishlists
   has_many :users, :through => :wishlists 
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  # has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
